d689244fab34ad9d707f241851485ae49f6fe83a
jhamile08 <jhamiledr222@gmail.com>
2024-11-01 20:10:24 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root "authentication/users#new" # Carga la vista de registro al iniciar
   get "index", to: "authentication/users#index"
   post "send_email/:link_type", to: "authentication/users#send_request_email", as: "send_email"
 
   namespace :authentication do
-    resources :users, only: [ :new, :create ] # Asegúrate de que aquí esté el espacio de nombres
-    resources :sessions, only: [ :new, :create ]
+    resources :users, only: [ :new, :create ] # Rutas para el registro
+    resources :sessions, only: [ :new, :create ] # Asegúrate de que 'sessions' esté definido
   end
 end
